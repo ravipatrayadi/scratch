1cd38e7e1b6b60c52cdfa93ff8e98356ac5b36e8
ravipatrayadi <ravipatrayadi@gmail.com>
2024-07-20 01:13:24 +0530

    Upload

---
+++ App.js
@@ -0,0 +1,42 @@
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const parent = React.createElement(
+    "div", {
+        id: "parent"
+    },
+    [
+        React.createElement(
+            "div", {
+                id: "child1"
+            }, [React.createElement("h1", {}, "Hello World1"),
+                React.createElement("h1", {}, "Hello World2")
+            ]
+        ),
+        React.createElement(
+            "div", {
+                id: "child2"
+            }, [
+                React.createElement("h1", {}, "Hello World3"),
+                React.createElement("h1", {}, "Hello World4")
+            ]
+        )
+    ]
+);
+
+
+root.render(parent);
+
+
+
+{
+
+
+
+
+
+    /* <div id = "parent"><div id = "child">
+            <h1 id = "myHeader1">hey1</h1>
+            <h1 id = "myHeader2">hey2</h1>
+        </div>
+    </div> */
+}
